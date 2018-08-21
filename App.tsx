b26c094d50d6f1875850edad05f68a42e8818983
jjrv <juha@busfaster.com>
2018-08-21 23:14:15 +0300

    Initial commit.

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1 class='test' style={{ color: 'red' }}>Hello, World!</h1>;
+
+ReactDOM.render(element, document.body);
